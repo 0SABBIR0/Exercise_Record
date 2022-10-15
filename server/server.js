558d7879999c197b6fcdd95f9c3b54b26a05f107
SABBIR AHAMMED <47359429+0SABBIR0@users.noreply.github.com>
2022-10-16 02:14:31 +0600

    Setting Up environment

--- server/server.js
+++ server/server.js
@@ -1,1 +1,20 @@
-const express = require('express');
+require('dotenv').config()
+const express=require('express');
+const workoutRoutes = require('./routes/workouts');
+//express app
+const app = express();
+//Middleware
+
+app.use((req,res,next)=>{
+    console.log(req.path,req.method);
+    next();
+})
+//Route
+app.get('/',(req,res)=>{
+    res.json({mssg:'Hello World'});
+});
+
+// Listen For Request
+app.listen(process.env.PORT,()=>{
+    console.log('Server Running on Port',process.env.PORT)
+}); 
